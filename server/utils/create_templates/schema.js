18b15334fe867f3fc7ebe14a57b98a7b02a85556
adamgoren <adamjgoren@gmail.com>
2019-11-06 16:24:39 -0500

    update table naming in server file

--- server/utils/create_templates/schema.js
+++ server/utils/create_templates/schema.js
@@ -1,78 +1,78 @@
-const tab = ` `;
-// let table = {
+const tab = ' ';
+// let objectType = {
 //     objTypeName: "User",
 //     fieldNames: ["id", "name"],
 //     fieldTypes: ["Int", "String"],
 // }
-let table1 = {
-    objTypeName: "User",
-    fieldNames: ["id", "name"],
-    fieldTypes: ["Int", "String"],
+const objectType1 = {
+  objTypeName: 'User',
+  fieldNames: ['id', 'name'],
+  fieldTypes: ['Int', 'String'],
+};
+const objectType2 = {
+  objTypeName: 'Pet',
+  fieldNames: ['id', 'name'],
+  fieldTypes: ['Int', 'String'],
+};
+
+const objectTypes = [objectType1, objectType2];
+
+function createSchema(objectTypes) {
+  let result = '';
+  result += `${tab}type Query {\n`;
+  for (const objectType of objectTypes) {
+    result += createRootQuery(objectType);
+  }
+  result += `${tab}}\n\n`;
+  result += `${tab}type Mutation {\n`;
+  for (const objectType of objectTypes) {
+    result += createMutation(objectType);
+  }
+  result += `${tab}}\n\n`;
+  for (const objectType of objectTypes) {
+    result += createObjType(objectType);
+  }
+
+  return result;
 }
-let table2 = {
-    objTypeName: "Pet",
-    fieldNames: ["id", "name"],
-    fieldTypes: ["Int", "String"],
+console.log(createSchema(objectTypes));
+
+
+function createRootQuery(objectType) {
+  const { objTypeName, fieldNames, fieldTypes } = objectType;
+  let result = '';
+  result += `${tab}${tab}getAll${objTypeName}: [${objTypeName}]\n`;
+  result += `${tab}${tab}get${objTypeName}ById(id: ID!): ${objTypeName}\n`;
+  return result;
+}
+// console.log(createRootQuery(objectType));
+
+function createMutation(objectType) {
+  const { objTypeName, fieldNames, fieldTypes } = objectType;
+  let result = '';
+  let args = '';
+  for (let i = 0; i < fieldNames.length; i++) {
+    args += `${fieldNames[i]}: ${fieldTypes[i]}, `;
+  }
+  result += `${tab}${tab}add${objTypeName}(${args}): Boolean\n`;
+  result += `${tab}${tab}update${objTypeName}(${args}): Boolean\n`;
+  result += `${tab}${tab}delete${objTypeName}(id: ID): Boolean\n`;
+  return result;
+}
+// console.log(createMutation(objectType));
+
+function createObjType(objectType) {
+  let result = '';
+  const { objTypeName, fieldNames, fieldTypes } = objectType;
+  result += `${tab}type ${objTypeName} {\n`;
+  for (let i = 0; i < fieldNames.length; i++) {
+    result += `${tab}${tab}${fieldNames[i]}: ${fieldTypes[i]}\n`;
+  }
+  result += `${tab}}\n\n`;
+  return result;
 }
 
-let tables = [table1, table2];
-
-function createSchema(tables){
-    let result =``;
-    result += `${tab}type Query {\n`;
-    for(let table of tables){
-        result += createRootQuery(table);
-    }
-    result += `${tab}}\n\n`;
-    result += `${tab}type Mutation {\n`;
-    for(let table of tables){
-        result += createMutation(table);
-    }
-    result += `${tab}}\n\n`;
-    for(let table of tables){
-        result += createObjType(table);
-    }
-
-    return result;
-}
-console.log(createSchema(tables));
-
-
-function createRootQuery(table){
-    const {objTypeName, fieldNames, fieldTypes} = table;
-    let result = ``;
-    result += `${tab}${tab}getAll${objTypeName}: [${objTypeName}]\n`
-    result += `${tab}${tab}get${objTypeName}ById(id: ID!): ${objTypeName}\n`
-    return result;
-}
-// console.log(createRootQuery(table));
-
-function createMutation(table){
-    const {objTypeName, fieldNames, fieldTypes} = table;
-    let result = ``;
-    let args = ``;
-    for(let i = 0; i < fieldNames.length; i++){
-        args += `${fieldNames[i]}: ${fieldTypes[i]}, `
-    }
-    result += `${tab}${tab}add${objTypeName}(${args}): Boolean\n`;
-    result += `${tab}${tab}update${objTypeName}(${args}): Boolean\n`;
-    result += `${tab}${tab}delete${objTypeName}(id: ID): Boolean\n`;
-    return result;
-}
-// console.log(createMutation(table));
-
-function createObjType(table){
-    let result = ``;
-    const {objTypeName, fieldNames, fieldTypes} = table;
-    result += `${tab}type ${objTypeName} {\n`
-    for(let i = 0; i < fieldNames.length; i++) {
-        result += `${tab}${tab}${fieldNames[i]}: ${fieldTypes[i]}\n`
-    }
-    result += `${tab}}\n\n`
-    return result;
-}
-
-    // console.log(createObjType(table));
+// console.log(createObjType(objectType));
 
 
 module.exports = createSchema;
