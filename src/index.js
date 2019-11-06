8a9d792842e804d6425e31975b91609ad30e5a5b
xosemanolo <xose.manolo.rodriguez@gmail.com>
2019-11-06 15:35:42 -0500

    restated index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
+import purple from '@material-ui/core/colors/purple';
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    // primary: purple,
+  },
+});
 
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('root'),
+);
 /*
 Co-authored-by: adamgoren <adamjgoren@gmail.com>
 Co-authored-by: apaisner <ampaisner@gmail.com>
