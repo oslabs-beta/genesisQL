3d1e4c6e21fb14705defaaf032820d8605a5c74d
Tom Herrmann <tomherrmanndev@gmail.com>
2019-11-09 15:51:52 -0500

    mongodb model cleanup

--- server/utils/models/mongodb.js
+++ server/utils/models/mongodb.js
@@ -1,16 +1,3 @@
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://fake:pass@cluster0-8hzwr.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.collection("devices").insertOne({ id: 1 }, (err, res) => {
-//     if (err) console.log('ERROR', err)
-//     console.log('THIS IS THE KONGODB RES', res)
-//   });
-//   // perform actions on the collection object
-//   client.close();
-// });
-
 const mongoose = require('mongoose');
 
 const myURI = 'mongodb+srv://fake:pass@cluster0-8hzwr.mongodb.net/test';
@@ -21,8 +8,6 @@ mongoose.connection.once('open', () => {
   console.log('Connected to DB');
 })
 
-const testSchema = new mongoose.Schema({ test: { type: 'String' } });
-
-const testModel = mongoose.model('Test', testSchema);
-
+// const testSchema = new mongoose.Schema({ test: { type: 'String' } });
+// const testModel = mongoose.model('Test', testSchema);
 // new testModel({ test: '1' }).save()
