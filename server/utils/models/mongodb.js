33ce78829b9ad623472cf83849783a09aeadc78c
Tom Herrmann <tomherrmanndev@gmail.com>
2019-11-09 15:50:02 -0500

    mongodb connected

--- server/utils/models/mongodb.js
+++ server/utils/models/mongodb.js
@@ -1,12 +1,28 @@
 
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://fake:pass@cluster0-8hzwr.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.collection("devices").insertOne({ id: 1 }, (err, res) => {
+//     if (err) console.log('ERROR', err)
+//     console.log('THIS IS THE KONGODB RES', res)
+//   });
+//   // perform actions on the collection object
+//   client.close();
+// });
 
-const uri = "mongodb+srv://fake:pass@cluster0-8hzwr.mongodb.net/test?retryWrites=true&w=majority";
+const mongoose = require('mongoose');
 
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const myURI = 'mongodb+srv://fake:pass@cluster0-8hzwr.mongodb.net/test';
 
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+mongoose.connect(myURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to DB');
+})
+
+const testSchema = new mongoose.Schema({ test: { type: 'String' } });
+
+const testModel = mongoose.model('Test', testSchema);
+
+// new testModel({ test: '1' }).save()
