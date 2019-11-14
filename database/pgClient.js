ab11257b5bd099d9704be584caa30db3ac7ad36a
adamgoren <adamjgoren@gmail.com>
2019-11-14 10:51:22 -0500

    use env

--- database/pgClient.js
+++ database/pgClient.js
@@ -1,23 +1,18 @@
 const { Pool } = require('pg');
-const queryString = require('./queryString.js')
+const queryString = require('./queryString.js');
+require('dotenv').config();
 
-const myURI = 'postgres://juqdonnj:URk1eALCbIPa5tgs29o2Tzs9YkvgL_yx@salt.db.elephantsql.com:5432/juqdonnj';
-
+const myURI = '';
 const URI = process.env.PG_URI || myURI;
 
 const pool = new Pool({ connectionString: URI });
 
 // creates users table
 pool.query(queryString.createUserTable, (err, result) => {
-    
-    if(err) console.error('FIRST error', err);
-    else {
-        console.log('TABLE users EXISTS')
-    }
+  if (err) console.error('FIRST error', err);
+  else {
+    console.log('TABLE users EXISTS');
+  }
 });
 
 module.exports = pool;
-
-
-
-
